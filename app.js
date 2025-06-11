b3943cd93743091d8393d86ab316837bcf53c813
amalra1 <amaral1@ufpr.br>
2025-06-11 12:10:33 -0300

    feat: Using yargs to deal with command line arguments

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
-import getNotes from './notes.js';
-import chalk from 'chalk';
+import getNotes from "./notes.js";
+import chalk from "chalk";
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
+
+const argv = yargs(hideBin(process.argv))
+  .command({
+    command: "add",
+    describe: "Adds a new note",
+    handler: function () {
+      console.log("Adding a new note");
+    },
+  })
+  .command({
+    command: "remove",
+    describe: "Removes a note",
+    builder: {
+      title: {
+        describe: "Title of the note to be removed",
+        demandOption: true,
+        type: "string",
+      },
+    },
+    handler: function (argv) {
+      console.log(`Removing the note titled: ${argv.title}`);
+    },
+  }).argv;
 
 const msg = getNotes();
 console.log(chalk.green(msg));
